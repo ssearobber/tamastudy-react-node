aa8fa2ee1fca15bf90a58c141a246fedac02a788
jonsoku2 <whdtjr2792@gmail.com>
2020-03-14 17:32:27 +0900

    2020년 3월 14일 스터디 - create post / get posts

--- jongseok/tamastudy-node/Routes/index.js
+++ jongseok/tamastudy-node/Routes/index.js
@@ -1,23 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('/', (req, res, next) => {
-  res.json({ success: true, message: '접속 성공 !' });
+router.get('/posts', async (req, res, next) => {
+  try {
+    let limit = 7;
+    if (req.query) {
+      limit = parseInt(req.query.limit);
+    }
+    const posts = await Post.find().limit(limit);
+    res.status(200).json({
+      success: true,
+      error: null,
+      result: posts,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      error: error.message,
+      result: null,
+    });
+  }
 });
 
-router.get('/post', (req, res, next) => {
-  //throw Error('hosting 에러!');
-  const post = [
-    {
-      id: 1,
-      title: 'node',
-    },
-    {
-      id: 2,
-      title: 'ruby',
-    },
-  ];
-  res.json({ success: true, data: post });
+router.post('/post/create', async (req, res, next) => {
+  try {
+    const post = await Post.create({
+      title: req.body.title,
+      description: req.body.description,
+      username: req.body.username,
+      imgUrl: req.body.imgUrl,
+    });
+    // const post = await Post.create({...req.body})
+    res.status(200).json({
+      success: true,
+      error: null,
+      data: post,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+      result: null,
+    });
+  }
 });
 
 module.exports = router;
--- jongseok/tamastudy-node/index.js
+++ jongseok/tamastudy-node/index.js
@@ -5,56 +5,34 @@ const morgan = require('morgan');
 const router = require('./routes/index');
 const errorResponse = require('./Middlewares/errorResponse');
 const colors = require('colors');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cors = require('cors');
+
 const app = express();
 
-const PORT = process.env.PORT;
-const MASTER = process.env.NAME;
+const PORT = 4000;
+const MASTER = 'JONGSEOK';
+const MONGO_URI =
+  'mongodb+srv://whdtjr2792:!canyou12@cluster0-mgk1n.gcp.mongodb.net/tamastudy-react-node?retryWrites=true&w=majority';
 
+mongoose
+  .connect(MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('DB접속 완료'.bgBlue.white))
+  .catch(error => {
+    console.log(error.bgRed.white);
+    process.exit(1);
+  });
+
+app.use(helmet());
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
 app.use(morgan('dev'));
-
-console.log(process.env.NAME);
-
 //! Routes
 app.use(router);
-
-// app.use((req, res, next) => {
-//   if (req.user === null) {
-//     console.log(req.user);
-//     return res.json({ success: false, data: {} });
-//   }
-//   console.log(req);
-//   next();
-// });
-
 //! error response middleware
 app.use(errorResponse);
 
 app.listen(PORT, () => {
   console.log(`${MASTER}의 서버에 접속했습니다. ${PORT}번 포트로 연결 되었습니다.`.bgWhite);
 });
-
-// const express = require('express');
-// const morgan = require('morgan');
-// require('dotenv').config();
-
-// const app = express();
-
-// // Dev logging middleware
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'));
-// }
-
-// app.get('/', (req, res) => {
-//   res.json({ success: true, message: '접속완료' });
-// });
-
-// const server = app.listen(process.env.PORT, () =>
-//   console.log(`${process.env.PORT}번으로 Node API 가동 중입니다. `),
-// );
-
-// // Handle unhandled promise rejections 처리되지 않은 약속 거부 처리
-// process.on('unhandledRejection', (err, promise) => {
-//   console.log(`Error: ${err.message}`.red);
-//   // Close server & exit process
-//   server.close(() => process.exit(1));
-// });
