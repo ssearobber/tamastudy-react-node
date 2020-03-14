1fd3f186a00fe729213b95e314fac8c125a5e8cc
jonsoku2 <whdtjr2792@gmail.com>
2020-03-15 05:21:46 +0900

    [node] deletePostById router

--- jongseok/tamastudy-node/Routes/index.js
+++ jongseok/tamastudy-node/Routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+// GET
+// getPosts
+// postman uri ex
+// http://localhost:4000/posts
 router.get('/posts', async (req, res, next) => {
   try {
     let limit = 7;
@@ -9,7 +13,6 @@ router.get('/posts', async (req, res, next) => {
       limit = req.query.limit;
     }
     const posts = await Post.find().limit(limit);
-    console.log(posts.length);
     res.status(200).json({
       success: true,
       error: null,
@@ -25,6 +28,10 @@ router.get('/posts', async (req, res, next) => {
   }
 });
 
+// POST
+// createPost
+// postman uri ex
+// http://localhost:4000/post/create
 router.post('/post/create', async (req, res, next) => {
   try {
     const post = await Post.create({
@@ -48,16 +55,23 @@ router.post('/post/create', async (req, res, next) => {
   }
 });
 
+// GET
+// getPostById
+// postman uri ex
+// http://localhost:4000/post/5e6c8fcbc66c3058b5fcf067
 router.get('/post/:postId', async (req, res, next) => {
   try {
     const post = await Post.findById({ _id: req.params.postId });
+    // post가 존재하지 않을 때
     if (!post) {
-      res.status(400).json({
+      return res.status(400).json({
         success: false,
         error: '포스트가 존재하지 않습니다.',
         result: null,
       });
     }
+    // post가 존재하였으므로 삭제되었고,
+    // result로 postId로 찾은 post를 내보낸다..
     res.status(200).json({
       success: true,
       error: null,
@@ -70,6 +84,38 @@ router.get('/post/:postId', async (req, res, next) => {
       error: error.message,
       result: null,
     });
+  }
+});
+
+// DELETE
+// deletePostById
+// postman uri ex
+// http://localhost:4000/post/delete/5e6c8fcbc66c3058b5fcf067
+router.delete('/post/delete/:postId', async (req, res, next) => {
+  try {
+    const post = await Post.findByIdAndDelete({ _id: req.params.postId });
+    // post가 존재하지 않을 때
+    if (!post) {
+      return res.status(400).json({
+        success: false,
+        error: '포스트가 존재하지 않습니다.',
+        result: null,
+      });
+    }
+    // post가 존재하였으므로 삭제되었고,
+    // result로 삭제완료 메시지를 보낸다.
+    res.status(200).json({
+      success: true,
+      error: null,
+      result: `${req.params.postID}번 포스트의 삭제가 완료되었습니다. `,
+    });
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      error: error.message,
+      result: null,
+    };
   }
 });
 
