38a064c7562a923bc6ec947145aac6daa6a31710
jonsoku2 <whdtjr2792@gmail.com>
2020-03-15 11:02:55 +0900

    [node] fix: 모든 controller에 asyncHandler 추가

--- jongseok/tamastudy-node/controllers/v1/user.controller.js
+++ jongseok/tamastudy-node/controllers/v1/user.controller.js
@@ -1,3 +1,4 @@
+const asyncHandler = require('../../middleware/asyncHandler');
 const User = require('../../database/models/User');
 const createJWT = require('../../util/user/createJWT');
 
@@ -6,94 +7,70 @@ const createJWT = require('../../util/user/createJWT');
 // signup
 // postman uri ex
 // http://localhost:4000/v1/user/signup
-exports.signup = async (req, res, next) => {
-  try {
-    const existingUser = await User.findOne({ email: req.body.email });
-    if (existingUser) {
-      return res.status(400).json({
-        success: false,
-        error: `${req.body.email}는 존재하는 이메일입니다. `,
-        data: null,
-      });
-    }
-    const newUser = await User.create({ ...req.body });
-    const token = await createJWT(newUser._id);
-    res.status(201).json({
-      success: true,
-      error: null,
-      data: token,
-    });
-  } catch (error) {
+exports.signup = asyncHandler(async (req, res, next) => {
+  const existingUser = await User.findOne({ email: req.body.email });
+  if (existingUser) {
     return res.status(400).json({
       success: false,
-      error: error.message,
+      error: `${req.body.email}는 존재하는 이메일입니다. `,
       data: null,
     });
   }
-};
+  const newUser = await User.create({ ...req.body });
+  const token = await createJWT(newUser._id);
+  res.status(201).json({
+    success: true,
+    error: null,
+    data: token,
+  });
+});
 
 // Public
 // post
 // signin
 // postman uri ex
 // http://localhost:4000/v1/user/signin
-exports.signin = async (req, res, next) => {
-  try {
-    const existingUser = await User.findOne({ email: req.body.email });
-    if (!existingUser) {
-      return res.status(400).json({
-        success: false,
-        error: `${req.body.email}는 존재하지 않는 이메일입니다. `,
-        data: null,
-      });
-    }
-    const isCorrectPassword = existingUser.matchPassword(req.body.password);
-    if (!isCorrectPassword) {
-      return res.status(401).json({
-        success: false,
-        error: `패스워드가 일치하지 않습니다. `,
-        data: null,
-      });
-    }
-    const token = await createJWT(existingUser._id);
-    res.status(200).json({
-      success: true,
-      error: null,
-      data: token,
-    });
-  } catch (error) {
+exports.signin = asyncHandler(async (req, res, next) => {
+  const existingUser = await User.findOne({ email: req.body.email });
+  if (!existingUser) {
     return res.status(400).json({
       success: false,
-      error: error.message,
+      error: `${req.body.email}는 존재하지 않는 이메일입니다. `,
       data: null,
     });
   }
-};
+  const isCorrectPassword = existingUser.matchPassword(req.body.password);
+  if (!isCorrectPassword) {
+    return res.status(401).json({
+      success: false,
+      error: `패스워드가 일치하지 않습니다. `,
+      data: null,
+    });
+  }
+  const token = await createJWT(existingUser._id);
+  res.status(200).json({
+    success: true,
+    error: null,
+    data: token,
+  });
+});
 
 // Private
 // get
 // users
 // postman uri ex
 // http://localhost:4000/v1/user/users
-exports.users = async (req, res, next) => {
-  try {
-    const users = await User.find()
-      .populate({
-        path: 'posts',
-        model: 'Post',
-      })
-      .select('-password');
-    console.log('he');
-    res.status(200).json({
-      success: true,
-      error: null,
-      data: users,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      success: false,
-      error: error.message,
-      data: null,
-    });
-  }
-};
+exports.users = asyncHandler(async (req, res, next) => {
+  const users = await User.find()
+    .populate({
+      path: 'posts',
+      model: 'Post',
+    })
+    .select('-password');
+  console.log('he');
+  res.status(200).json({
+    success: true,
+    error: null,
+    data: users,
+  });
+});
