feb336021abb3571057668a44ac12562af8db39f
jonsoku2 <whdtjr2792@gmail.com>
2020-03-15 09:36:44 +0900

    [node] feat: signin 라우트 및 컨트롤러 추가

--- jongseok/tamastudy-node/Routes/v1/user.route.js
+++ jongseok/tamastudy-node/Routes/v1/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { signup } = require('../../controllers/v1/user.controller');
+const { signup, signin } = require('../../controllers/v1/user.controller');
 
 const router = express.Router();
 
 // 컨트롤러를 불러온다. (즉, 이전에 작성했던 '로직'만 컨트롤러로 옮겼다고 생각하면 된다.)
 
 router.route('/signup').post(signup);
+router.route('/signin').post(signin);
 
 module.exports = router;
--- jongseok/tamastudy-node/controllers/v1/user.controller.js
+++ jongseok/tamastudy-node/controllers/v1/user.controller.js
@@ -17,6 +17,43 @@ exports.signup = async (req, res, next) => {
     }
     const newUser = await User.create({ ...req.body });
     const token = await createJWT(newUser._id);
+    res.status(201).json({
+      success: true,
+      error: null,
+      data: token,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error: error.message,
+      data: null,
+    });
+  }
+};
+
+// post
+// signin
+// postman uri ex
+// http://localhost:4000/v1/user/signin
+exports.signin = async (req, res, next) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (!existingUser) {
+      return res.status(400).json({
+        success: false,
+        error: `${req.body.email}는 존재하지 않는 이메일입니다. `,
+        data: null,
+      });
+    }
+    const isCorrectPassword = existingUser.matchPassword(req.body.password);
+    if (!isCorrectPassword) {
+      return res.status(401).json({
+        success: false,
+        error: `패스워드가 일치하지 않습니다. `,
+        data: null,
+      });
+    }
+    const token = await createJWT(existingUser._id);
     res.status(200).json({
       success: true,
       error: null,
